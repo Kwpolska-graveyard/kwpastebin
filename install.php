6309231a41ec61ffe42940e678a3e7568397501f
Kwpolska <kwpolska@gmail.com>
2010-07-09 05:01:22 -0700

    updates

--- install.php
+++ install.php
@@ -12,11 +12,10 @@ try
 				`language` VARCHAR( 50 ) NOT NULL ,
 				`timestamp` VARCHAR( 50 ) NOT NULL
 				) ENGINE = MYISAM');
-		echo 'Congratulations. It seems like it worked. It finished with (1 = installed): '.$stmt;
+		if ($stmt == 0) echo 'It failed. (code: 0)' else echo 'Congratulations. (code: 1)';
 	}
 catch(PDOException $e)
 	{
-		echo 'ERROR: ' . $e->getMessage();
-		echo '\n<br />That means, it isn\'t installed. Did you modify the config.php correctly?';
+		echo 'It failed. (code: none), error message:' . $e->getMessage();
 	}
 ?>
