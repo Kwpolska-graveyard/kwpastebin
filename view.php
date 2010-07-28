9b576ba6f91dd989941adcaf2b1375f56e1b1ac9
Kwpolska <kwpolska@gmail.com>
2010-07-28 14:23:18 +0200

    wrong syntax

--- view.php
+++ view.php
@@ -17,7 +17,7 @@ try
 		$pdo = new PDO(DB_DSN, DB_USR, DB_PWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       
-		$stmt = $pdo -> query('SELECT code, language FROM `'.DB_TNM.'` WHERE `timestamp` =?');
+		$stmt = $pdo -> prepare('SELECT code, language FROM `'.DB_TNM.'` WHERE `timestamp` = ?');
 		try {
 			  $stmt->execute(array($_GET['id']));
 
