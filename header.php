4a90f460134364d2d9c0b6327db890876aa2b669
Kwpolska <kwpolska@gmail.com>
2010-07-30 20:14:57 +0200

    small bugfix

--- header.php
+++ header.php
@@ -5,7 +5,7 @@ include_once './config.php';
 echo '<!DOCTYPE html>
 <meta charset="UTF-8">
 <link rel="Stylesheet" type="text/css" href="./style.css">
-<title>Kw\'s Pastebin</title>
+<title>'.KP_NME.'</title>
 <!-- Kw\'s Pastebin
 Copyright Kwpolska 2010. Licensed on GPLv2. -->
 <h1 id="head">'.KP_NME.'</h1>
