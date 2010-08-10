9778924c63a509c545b183a2ec97d1c9c6b55f5e
Kwpolska <kwpolska@gmail.com>
2010-08-11 00:00:18 +0200

    auto commit

--- view.php
+++ view.php
@@ -10,17 +10,12 @@ try
 		$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       
 		$stmt = $pdo -> prepare('SELECT code, language FROM `'.DB_TNM.'` WHERE `timestamp` = ?');
-		try {
-			  $stmt->execute(array($_GET['id']));
-
-			  $obj = $stmt->fetch(PDO::FETCH_OBJ);
-			echo '<a href="./plain.php?id='.$_GET['id'].'">plaintext</a> - <a href="./dl.php?id='.$_GET['id'].'">download</a> - hilighted by <a href="http://qbnz.com/highlighter/">GeSHi</a><br>';
-			$geshi = new GeSHI($obj->code, $obj->language);
-			$geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS, 2); $geshi->set_line_style('background: #fcfcfc;', 'background: #f0f0f0;'); //this determines the line styles; even = grey, odd - white-ish.
-			echo $geshi->parse_code();
-		} catch(PDOException $e){
-			  echo 'ERROR: ' . $e->getMessage();
-		}
+		  $stmt->execute(array($_GET['id']));
+		  $obj = $stmt->fetch(PDO::FETCH_OBJ);
+		echo '<a href="./plain.php?id='.$_GET['id'].'">plaintext</a> - <a href="./dl.php?id='.$_GET['id'].'">download</a> - hilighted by <a href="http://qbnz.com/highlighter/">GeSHi</a><br>';
+		$geshi = new GeSHI($obj->code, $obj->language);
+		$geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS, 2); $geshi->set_line_style('background: #fcfcfc;', 'background: #f0f0f0;'); //this determines the line styles; even = grey, odd - white-ish.
+		echo $geshi->parse_code();
 		$stmt -> closeCursor();
 	}
 catch(PDOException $e)
