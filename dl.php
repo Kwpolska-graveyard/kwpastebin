4892fd6f2b8443bc7c00e5d91c9c0db54af89b54
Kwpolska <kwpolska@gmail.com>
2011-01-20 17:20:51 +0100

    oops...

--- dl.php
+++ dl.php
@@ -1,6 +1,6 @@
 <?php
 include_once './config.php';
-header('Content-type: text/plain');
+header('Content-Type: text/plain');
 header('Content-Disposition: attachment; filename="'.KP_FLN.'.txt"');
 //KwPastebin
 //Copyright Kwpolska 2010. Licensed on GPLv3.
