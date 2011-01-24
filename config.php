ae236f17af0023d9bd468cc35afa3555d5fcc6b2
Kwpolska <kwpolska@gmail.com>
2011-01-24 17:29:09 +0100

    auto commit

--- config.php
+++ config.php
@@ -17,7 +17,7 @@ $dbdsn = 'mysql:host='.$dbhst.';dbname='.$dbnme; // change mysql if needed
 
 /// DYNAMIC SETTINGS ///
 function savant() {
-   global $content;
+   global $content, $sitename;
    include_once 'Savant3.php';
    $tpl = new Savant3();
    $tpl->setEscape();
